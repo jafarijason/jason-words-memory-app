e4cc8d06c7f10334996f0edaa4c808eb0d3a18d3
Jafari Jason <eng.jafari.m@gmail.com>
2021-03-14 03:27:37 -0400

    add image and button

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import logo from './assets/logo.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{color: '#888', fontSize: 18}}> 
+      
+      <Image source={logo} style={{ width: 305, height: 159 }} />
+      {/* <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }}   style={styles.logo} /> */}
+      
+      <Text style={styles.instructions}>
         To share a photo from your phone with a friend, just press the button below!
       </Text>
+      <TouchableOpacity
+        onPress={() => alert('Hello, world!')}
+        style={styles.button}>
+        <Text  style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -18,5 +28,23 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 10,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+  },  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
   },
 });

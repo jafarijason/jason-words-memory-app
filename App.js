589092dbd14d66631a9fa8fd90f90c53f7bd5f94
Jafari Jason <eng.jafari.m@gmail.com>
2021-03-14 11:19:13 -0400

    picking an iimage

--- App.js
+++ App.js
@@ -1,9 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import logo from './assets/logo.png';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+
+  const [selectedImage, setSelectedImage] = useState(null);
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setSelectedImage({ localUri: pickerResult.uri });
+  }
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: selectedImage.localUri }}
+          style={styles.thumbnail}
+        />
+      </View>
+    );
+  }
+  
   return (
     <View style={styles.container}>
       
@@ -14,7 +44,7 @@ export default function App() {
         To share a photo from your phone with a friend, just press the button below!
       </Text>
       <TouchableOpacity
-        onPress={() => alert('Hello, world!')}
+        onPress={openImagePickerAsync}
         style={styles.button}>
         <Text  style={styles.buttonText}>Pick a photo</Text>
       </TouchableOpacity>
@@ -47,4 +77,9 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: '#fff',
   },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain"
+  }
 });
